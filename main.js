f621c3278502aeea4a5374d40bb71af778d756ce
Thi Nguyen <tttn.tn13@gmail.com>
2021-05-26 13:47:33 -0700

    update

--- main.js
+++ main.js
@@ -48,19 +48,25 @@ const gameBoard = (() => {
         DRAW: 3,
         NORESULT: 4,
     }
+    const display_winner = document.getElementById('display-winner');
     
     const check_for_winner = () => {
         let length = board.length;
-        
         if ((_ifVertical(length, gameController.player_marker)) || (_ifHorizontal(length,gameController.player_marker)) || (_ifDiagonal(length, gameController.player_marker))) { 
             console.log("Player won");
+            display_winner.style.display = 'block';
+            display_winner.innerHTML = "CONGRATS! YOU WON!"
             won_already = true;
             return game_status.WIN;
         } else if (check_if_filled()) {
             console.log("draw");
+            display_winner.style.display = 'block';
+            display_winner.innerHTML = "IT'S A TIE!"
             won_already = true;
             return game_status.DRAW;
         } else if ((_ifVertical(length, gameController.computer_marker)) || (_ifHorizontal(length,gameController.computer_marker)) || (_ifDiagonal(length, gameController.computer_marker))) {  
+            display_winner.style.display = 'block';
+            display_winner.innerHTML  = "SORRY! YOU LOSE!"
             won_already = true;
             return game_status.LOSE;
         } else {
@@ -104,7 +110,7 @@ const gameBoard = (() => {
                 iswinning = iswinning && (get_marker(x,y) === marker);
                 // horizontal_cells[x].push(get_marker(x,y));
             }
-            if (iswinning) {
+            if (iswinning) { 
                 return iswinning;
             }
         }
@@ -140,11 +146,14 @@ const gameBoard = (() => {
         board_size,
         check_if_filled,
         game_status,
+        display_winner,
     };
 })();
 
 const displayController = (() => {
     const draw_board = () => {
+        
+        gameBoard.display_winner.style.display = 'none';
         const mytable = document.querySelector('table');
         mytable.innerHTML = "";
         for (let i =0; i < 3; i++) {
@@ -182,20 +191,31 @@ const displayController = (() => {
     }
 
     const draw_scores = (player_score,tie_score,computer_score) => {
+        console.log("drawing these scores",player_score,tie_score,computer_score );
         const player_score_p = document.getElementById('player-score');
-        player_score_p.innerHTML = `${player_score}`;
+        if (player_score == null ) {
+            player_score_p.innerHTML = "0";
+        } else {
+            player_score_p.innerHTML = `${player_score}`;
+        }
 
         const computer_score_p = document.getElementById('computer-score');
-        computer_score_p.innerHTML = `${computer_score}`;
+        if (computer_score == null ) {
+            computer_score_p.innerHTML = "0";
+        } else {
+            computer_score_p.innerHTML = `${computer_score}`;
+        }
 
-        const tie_p = document.getElementById('tie-score');
-        tie_p.innerHTML = `${tie_score}`;
+        const tie_score_p = document.getElementById('tie-score');
+        if (tie_score == null ) {
+            tie_score_p.innerHTML = "0";
+        } else {
+            tie_score_p.innerHTML = `${tie_score}`;
+        }
     }
 
     return {
-        draw_board,    
-        start_new_game,
-        draw_scores,
+        draw_board, start_new_game, draw_scores,
     };
 
 })();
@@ -203,31 +223,50 @@ const displayController = (() => {
 const gameController = (() => {
     const player_marker = "x";
     const computer_marker = "o";
-    displayController.draw_board();
-    
+        
+    const loadfromStorage = () => {
+        playerScoreFromStorage = JSON.parse(localStorage.getItem("playerScore"));
+        computerScoreFromStorage = JSON.parse(localStorage.getItem("computerScore"));
+        tieScoreFromStorage = JSON.parse(localStorage.getItem("tieScore"));
+        if ((playerScoreFromStorage !== null) || (computerScoreFromStorage != null) || (tieScoreFromStorage != null)){
+            player_score = playerScoreFromStorage;
+            computer_score = computerScoreFromStorage;
+            tie_score = tieScoreFromStorage;
+        } else if ((playerScoreFromStorage == null) && (computerScoreFromStorage == null) &&  (tieScoreFromStorage == null)) {
+            player_score = 0;
+            computer_score = 0;
+            tie_score = 0;
+        }
+        displayController.draw_scores(playerScoreFromStorage,tieScoreFromStorage,computerScoreFromStorage);
+    }
+
     let player_score = 0;
     let computer_score = 0;
-    let tie = 0;
-
+    let tie_score = 0;
     const game_is_played = (player, row, column) => {
         if (player == player_marker) {
             gameBoard.insert_marker(player,row,column);
         } else {
             computerController.insert_marker(gameController.computer_marker);
         }
+
         displayController.draw_board();
+
         let result = gameBoard.check_for_winner();
         if (result == gameBoard.game_status.WIN) {
             player_score += 1;
+            localStorage.setItem("playerScore", JSON.stringify(player_score));
         } else if (result == gameBoard.game_status.DRAW) {
-            tie += 1;
+            tie_score += 1;
+            localStorage.setItem("tieScore", JSON.stringify(tie_score));
         } else if (result == gameBoard.game_status.LOSE) {
             computer_score += 1;
+            localStorage.setItem("computerScore", JSON.stringify(computer_score));
         }
-        displayController.draw_scores(player_score,tie,computer_score);
-    }
 
-    return {player_marker,computer_marker,game_is_played};
+        displayController.draw_scores(player_score,tie_score,computer_score);
+    }
+    return {player_marker,computer_marker,game_is_played,loadfromStorage};
 })();
 
 const computerController = (() => {
@@ -245,3 +284,6 @@ const computerController = (() => {
 
     return { insert_marker};
 })();
+
+displayController.draw_board();
+gameController.loadfromStorage();
